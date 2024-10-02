7ed5cc71503add8a0a50b1f024a0454963253b62
ankit.jangir@jploft.in <ankit.jangir@jploft.in>
2024-10-02 16:04:29 +0530

    latest

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,20 @@ const router = express.Router();
 const userController = require("../controller/userController");
 const isAuthenticated = require("../middleware/isAuthanticated");
 
-
-router.post("/signup", userController.signup)
+// Public routes
+router.post("/signup", userController.signup);
 router.post("/signin", userController.signInWithPassword);
 router.post('/signin/otp', userController.signInWithOTP);
 router.post('/verify/otp', userController.verifyOTP);
+router.get("/verify-email", userController.verifyEmail);
+
+// Forgot password and reset password 
+router.post('/forgotPassword', userController.forgotPassword);
+router.post("/resetPassword", userController.resetPassword);
+
+// Protected routes (require authentication)
+router.post("/editProfile", isAuthenticated, userController.editProfile);
+router.get("/logout", isAuthenticated, userController.logout); 
+router.patch("/delete", isAuthenticated, userController.deactivateUser)
 
 module.exports = router;
