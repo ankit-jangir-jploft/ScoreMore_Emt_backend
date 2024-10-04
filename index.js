78fc5c9308832ada82c5b0ae6aac4aa6cd19f847
ankit.jangir@jploft.in <ankit.jangir@jploft.in>
2024-10-04 10:28:58 +0530

    update profile pic

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
-require("dotenv").config();
-const port = process.env.PORT || 5000; // Default to port 5000 if not set
 const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const path = require('path')
-
-// Routes
+const path = require('path');
 const userRoutes = require("./routes/userRoutes");
+const questionRoutes = require("./routes/questionRoute");
 const connectDb = require("./utils/db");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: "http://localhost:5173", // Your frontend originonsole.log
+  origin: "http://localhost:5173", // Your frontend origin
   credentials: true, // Allow credentials (cookies, etc.)
 };
 
 // Middleware
-app.use(cors(corsOptions)); // Enable CORS with specified options
-app.use(express.json()); // Body parser for JSON
-app.use(cookieParser()); // Cookie parser
-app.use(bodyParser.json({ limit: "1024mb" })); // Set limits for JSON body
-app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true })); // Set limits for URL-encoded body
-app.use(morgan("dev")); // Logging middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "1024mb" }));
+app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }));
+app.use(morgan("dev"));
 
+// Static files
+console.log("____dirname", path.join(__dirname))
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+// Routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/question", questionRoutes);
 
-app.use('/assets', express.static(path.join(__dirname, 'middleware', 'assets')));
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ status: "Error", message: err.message });
 });
 
-// User routes
-app.use("/api/v1/user", userRoutes);
-
 // Database connection and server startup
-app.listen(port, async (err) => {
+app.listen(port, async () => {
   await connectDb();
-  if (err) {
-    console.log("Server not connected");
-  }
   console.log(`Server is connected on port ${port}`);
 });
