3ea56f155285105e8bc26a9f30eeee44bda2d37d
ankit-jangir-jploft <ankit.jangir@jploft.in>
2024-10-18 10:02:54 +0530

    rolve

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const mongoose = require("mongoose");
 // require('dns').setDefaultResultOrder('ipv4first');
 
@@ -32,19 +31,3 @@ const connectDb = async () => {
 }
 
 module.exports = connectDb;
-=======
-const mongoose = require("mongoose");
-require('dns').setDefaultResultOrder('ipv4first');
-
-
-const connectDb = async ()=> {
-    try {
-        await mongoose.connect("mongodb+srv://ankitjangir:010720024@cluster0.pnzca.mongodb.net/scoremore?retryWrites=true&w=majority");
-        console.log("database connect successfully !!")
-    } catch (err) {
-        console.log("err", err)
-    }
-}
-
-module.exports = connectDb;
->>>>>>> a454120087c0035eab799d922da9529ac04eb33f
