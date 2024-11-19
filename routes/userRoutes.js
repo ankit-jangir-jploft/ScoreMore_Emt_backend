b7c692d46e0834e10efb0e211b3b30d048376167
ankit-jangir-jploft <ankit.jangir@jploft.in>
2024-11-19 19:12:30 +0530

    reminder

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,7 +26,7 @@ router.post("/logout", isAuthenticated, userController.logout);
 
 
 
-// User question route
+// User question routes
 router.post("/userQuestionData", isAuthenticated, userController.userQuestionData);
 router.post("/findquestionStatus", userController.findquestionMarkSatatus);
 router.post("/updateQuestion", userController.updateQuestionData)
